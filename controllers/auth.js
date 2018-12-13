c159d5992bdfae4d72c8a52c967196c4c1abf023
jeff-mccommas <jeffmccommas@gmail.com>
2018-12-13 13:42:21 -0500

    added sign-up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,29 +1,39 @@
-const User = require("../models/user");
+const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-  res.render("auth/login", {
-    path: "/login",
-    pageTitle: "Login",
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
     isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById("5c118fba4b6237fdf4c899fb")
+  User.findById('5bab316ce0a7c75f783cb8a8')
     .then(user => {
       req.session.isLoggedIn = true;
       req.session.user = user;
       req.session.save(err => {
         console.log(err);
-        res.redirect("/");
+        res.redirect('/');
       });
     })
     .catch(err => console.log(err));
 };
 
+exports.postSignup = (req, res, next) => {};
+
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
     console.log(err);
-    res.redirect("/");
+    res.redirect('/');
   });
 };
