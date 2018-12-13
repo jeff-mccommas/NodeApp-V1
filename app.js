88977c2c34af36d81976ca7b846fcf441b36cac1
jeff-mccommas <jeffmccommas@gmail.com>
2018-12-13 09:32:48 -0500

    using-mongodb-session-store

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
+const MONGODB_URI =
+  "mongodb+srv://jmccommas:Monster2018!@cluster0-xqdtj.mongodb.net/shop";
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions"
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -18,9 +27,17 @@ const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
-  User.findById("5c118fba4b6237fdf4c899fb")
+  User.findById("5bab316ce0a7c75f783cb8a8")
     .then(user => {
       req.user = user;
       next();
@@ -35,9 +52,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    "mongodb+srv://jmccommas:Monster2018!@cluster0-xqdtj.mongodb.net/shop?retryWrites=true"
-  )
+  .connect(MONGODB_URI)
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
